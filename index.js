e4f9db16091d8bb903d9a11b6b20703b92521fbb
seonghojoo <etkl8888@naver.com>
2022-03-10 23:55:51 +0900

    db

--- index.js
+++ index.js
@@ -13,9 +13,4 @@ app.use(bodyParser.urlencoded({extended:true})) // for parsing application
 
 app.use('/users', user);
 
-
-app.listen(3000,function(){
-    console.log('server is running');
-})
-
 module.exports = app;
